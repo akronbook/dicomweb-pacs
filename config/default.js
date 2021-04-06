a2f3cd8da16d12294b2f110b47097f8777e081f2
Yuanming Chen <yuanming@hotmail.com>
2021-04-05 19:37:41 -0600

    Define port

--- config/default.js
+++ config/default.js
@@ -13,7 +13,7 @@ const config = {
       }],
     logDir: "./logs",
     storagePath: "./data",
-    webserverPort: 5000,
+    webserverPort: process.env.PORT || 5000,
     qidoMinChars: 0, // do not issue c-find if search contains less characters
     qidoAppendWildcard: true, // auto append * for patient name query
     permissiveMode: true, // when set to false, all AETs able to query and push need to be in peers
